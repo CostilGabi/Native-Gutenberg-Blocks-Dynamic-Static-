c964fb55335d152f57500edcddd2eb3fa069d937
Costil Gabriel <37557421+CostilGabi@users.noreply.github.com>
2023-11-23 11:09:02 +0200

    removed build folder

--- static-blocks/build/fp-pt-artisans/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'ede010b3d1f553d527b1');
--- static-blocks/build/fpthree-pt/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'fb367d923dcf155aed72');
